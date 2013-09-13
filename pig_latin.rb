fe34920171779279a2c54c1ab4f7e273ec02c270
Adam Barber <adam@adambarber.tv>
2013-09-12 23:15:39 -0700

    minor refactoring

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,13 +1,20 @@
 class PigLatin
   VOWELS = ['a', 'e', 'i', 'o', 'u']
+
   def self.translate(english)
-    english.split(' ').collect do |fragment|
-      word, punctuation = fragment.match(/([a-zA-Z]+)(\W*)/).captures
-      "#{translate_word_to_pig_latin(word)}#{punctuation}"
-    end.join(' ')
+    perform_translation(english)
   end
 
   private
+
+  def self.perform_translation(english)
+    english.split(' ').collect {|fragment| process_phrase(fragment) }.join(' ')
+  end
+
+  def self.process_phrase(fragment)
+    word, punctuation = fragment.match(/([a-zA-Z]+)(\W*)/).captures
+    "#{translate_word_to_pig_latin(word)}#{punctuation}"
+  end
 
   def self.translate_word_to_pig_latin(english_word)
     first_letter = english_word[0]
@@ -17,10 +24,6 @@ class PigLatin
 
   def self.translate_single_english_word(english_word, first_letter)
     base_string = english_word[1..-1]
-    if VOWELS.include?(first_letter)
-      "#{english_word}ay"
-    else
-      "#{base_string}#{first_letter}ay"
-    end
+    VOWELS.include?(first_letter) ? "#{english_word}ay" : "#{base_string}#{first_letter}ay"
   end
 end
